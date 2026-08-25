cb3aeb3be629192e5ca2a2623752c1128c8a35d8
Priyanka Panda <158509146+PriyankaPanda09@users.noreply.github.com>
2026-08-25 16:25:14 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.java
@@ -0,0 +1,16 @@
+// this is adavanced version of previous one this is timecomplexity o(n) and space complexity(1);
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] output = new int[nums.length];
+        output[0] = 1;
+        for (int i=1;i<nums.length;i++){
+            output[i] = output[i-1] * nums[i-1];
+        }
+        int suffix = 1;
+         for(int i = nums.length-1;i>=0;i--){
+            output[i]*= suffix;
+            suffix*= nums[i];
+         }
+        return output;
+    }
+}  
